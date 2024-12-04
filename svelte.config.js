b86e1c8ae7e6752c429cec3298e6f4adf540404d
Maksym Babii <maksymbabiiwork@gmail.com>
2024-12-04 09:50:37 +0200

    updated svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,18 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>'],
-      },
-      platformProxy: {
-        configPath: 'wrangler.toml',
-        environment: undefined,
-        experimentalJsonConfig: false,
-        persist: false,
-      },
-    }),
+    adapter: adapter(),
   },
 };
 
